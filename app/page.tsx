b658f36258157b6b8a8e77149bc3360d4fadcdab
Paulo Alex <garbagames@gmail.com>
2024-12-30 21:11:42 -0300

    chore: add husky and lint staged setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
-export default function Home() {
+const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <h1>Home</h1>
     </div>
   );
-}
+};
+
+export default Home;

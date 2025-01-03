a1363805413add672cab7e4e09bdf83c8908445d
Paulo Alex <garbagames@gmail.com>
2025-01-03 18:32:02 -0300

    refactor: ensure product list receives oridycts as props

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,25 @@ import Search from './_components/search-input'
 import ProductList from './_components/product-list'
 import { Button } from './_components/ui/button'
 import { ChevronRightIcon } from 'lucide-react'
+import { db } from './_lib/prisma'
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discount_percentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  })
+
   return (
     <>
       <Header />
@@ -41,7 +58,7 @@ const Home = () => {
             <ChevronRightIcon size={16} />
           </Button>
         </div>
-        <ProductList />
+        <ProductList products={products} />
       </div>
     </>
   )
